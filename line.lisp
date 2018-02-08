905f68d2227dd5ce5771cdbe8e223c232a667a51
kchen29 <kchen29@stuy.edu>
2018-02-08 11:23:04 -0500

    more framework

--- line.lisp
+++ line.lisp
@@ -12,13 +12,16 @@
 
 ;;screen is a 2d array of pixels
 ;;pixel is a '(r g b)
+;;assume octant I for now
 (defun draw-line (x0 y0 x1 y1 screen pixel)
   )
 
 ;;draws a-size x a-size image
 (defun main (a-size)
-  (let ((screen (make-array (list a-size a-size)))
-        (color '(0 255 0)))
-    (draw-line 249 0 0 249 screen color))))
+  (let* ((dimensions (list a-size a-size))
+         (screen (make-array dimensions :initial-element '(0 0 0)))
+         (color '(0 255 0)))
+    (draw-line 249 0 0 249 screen color)
+    (write-ppm "output.ppm" dimensions screen)))
 
 (main 500)
